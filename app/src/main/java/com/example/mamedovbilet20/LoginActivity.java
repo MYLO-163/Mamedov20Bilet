a61b0b48110705d900df8e5769757e1d45836625
starterov <starterror@duck.com>
2023-12-28 15:34:47 +0700

    First commit

--- app/src/main/java/com/example/mamedovbilet20/LoginActivity.java
+++ app/src/main/java/com/example/mamedovbilet20/LoginActivity.java
@@ -30,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
                 String password = etPassword.getText().toString();
 
 
-                if (etLogin !=null) {
+                if (password!=null) {
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                 } else {
